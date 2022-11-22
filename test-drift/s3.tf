b3936d8e919a812530beffcdb7b8f5d7686fc247
z <37553582+ziyu-yu@users.noreply.github.com>
2022-11-21 21:00:36 -0800

    Create s3.tf

---
+++ test-drift/s3.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "dockingbay" {
+  bucket_prefix = "test-drift-bucket-"
+
+  tags = {
+    Name                 = "Drift Test"
+    Environment          = "Dev"
+  }
+}
