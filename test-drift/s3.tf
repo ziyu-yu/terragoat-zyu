1898b8745b5578e755fce244eabbae31c3b85684
z <37553582+ziyu-yu@users.noreply.github.com>
2022-11-22 10:17:45 -0800

    Update s3.tf

--- test-drift/s3.tf
+++ test-drift/s3.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2" 
 }
 
 resource "aws_s3_bucket" "dockingbay" {
-  bucket_prefix = "test-drift-bucket-"
+  bucket_prefix = "test-drift-bucket-" 
 
   tags = {
     Name                 = "Drift Test"
